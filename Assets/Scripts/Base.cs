b1c5ff5af2fb9ed37a53b5f67828e1ec41529f45
v1to <pixel2006.aa@gmail.com>
2023-11-29 22:28:51 +0200

    Multiple flags bug fix

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -6,134 +6,134 @@ using UnityEngine.EventSystems;
 [RequireComponent(typeof(MouseFollow))]
 public class Base : MonoBehaviour, IPointerClickHandler
 {
-	[SerializeField] private Unit _unitPrefab;
-	[SerializeField] private Flag _flagPrefab;
-	[SerializeField] private Ground _ground;
-	[SerializeField] private ResourceScanner _resourceScanner;
-	[SerializeField] private SpawnPoint[] _unitSpawnPoints;
+    [SerializeField] private Unit _unitPrefab;
+    [SerializeField] private Flag _flagPrefab;
+    [SerializeField] private Ground _ground;
+    [SerializeField] private FlagSelector _flagSelector;
+    [SerializeField] private ResourceScanner _resourceScanner;
+    [SerializeField] private SpawnPoint[] _unitSpawnPoints;
 
-	private MouseFollow _mouseFollow;
-	private Queue<Unit> _activeUnits;
-	private Coroutine _mouseFollowCoroutine;
+    private MouseFollow _mouseFollow;
+    private Queue<Unit> _activeUnits;
+    private Coroutine _mouseFollowCoroutine;
 
-	private Flag _flag;
-	private Flag _selectedFlag;
+    private Flag _flag;
+    private Flag _selectedFlag;
 
-	private bool _flagSelected;
+    private int _resourcesCount = 0;
+    private int _startUnitsCount = 3;
+    private int _unitSpawnPointIndex = 0;
+    private int _unitPrice = 3;
+    private int _baseBuildPrice = 5;
 
-	private int _resourcesCount = 0;
-	private int _startUnitsCount = 3;
-	private int _unitSpawnPointIndex = 0;
-	private int _unitPrice = 3;
-	private int _baseBuildPrice = 5;
+    private void Awake()
+    {
+        _activeUnits = new Queue<Unit>();
+        _mouseFollow = GetComponent<MouseFollow>();
 
-	private void Awake()
-	{
-		_activeUnits = new Queue<Unit>();
-		_mouseFollow = GetComponent<MouseFollow>();
+        _ground.Clicked += TryPlaceFlag;
 
-		_ground.Clicked += TryPlaceFlag;
+        for (int i = 0; i < _startUnitsCount; i++)
+        {
+            CreateUnit();
+        }
 
-		for (int i = 0; i < _startUnitsCount; i++)
-		{
-			CreateUnit();
-		}
+        StartCoroutine(SendUnitsForResources());
+    }
 
-		StartCoroutine(SendUnitsForResources());
-	}
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (_selectedFlag != null && _flagSelector.FlagSelected)
+        {
+            StopCoroutine(_mouseFollowCoroutine);
+            Destroy(_selectedFlag.gameObject);
+            _flagSelector.SetFlagStatus(false);
+        }
+        else if (_flagSelector.FlagSelected == false)
+        {
+            _selectedFlag = Instantiate(_flagPrefab);
+            _mouseFollowCoroutine = StartCoroutine(_mouseFollow.FollowMouse(_selectedFlag.transform));
+            _flagSelector.SetFlagStatus(true);
+        }
+    }
 
-	public void OnPointerClick(PointerEventData eventData)
-	{
-		if (_selectedFlag != null)
-		{
-			StopCoroutine(_mouseFollowCoroutine);
-			Destroy(_selectedFlag.gameObject);
-			_flagSelected = false;
-		}
-		else
-		{
-			_selectedFlag = Instantiate(_flagPrefab);
-			_mouseFollowCoroutine = StartCoroutine(_mouseFollow.FollowMouse(_selectedFlag.transform));
-			_flagSelected = true;
-		}
-	}
+    public void CollectResource(Unit unit, Resource deliveredResource)
+    {
+        Destroy(deliveredResource.gameObject);
+        _resourcesCount++;
+        _activeUnits.Enqueue(unit);
+        MakeNewStep();
+    }
 
-	public void CollectResource(Unit unit, Resource deliveredResource)
-	{
-		Destroy(deliveredResource.gameObject);
-		_resourcesCount++;
-		_activeUnits.Enqueue(unit);
-		MakeNewStep();
-	}
+    public void AddUnit(Unit unit)
+    {
+        _activeUnits.Enqueue(unit);
+    }
 
-	public void AddUnit(Unit unit)
-	{
-		_activeUnits.Enqueue(unit);
-	}
+    private void MakeNewStep()
+    {
+        if (_flag == null && _resourcesCount >= _unitPrice)
+        {
+            BuyNewUnit();
+        }
+        else if (_resourcesCount >= _baseBuildPrice && _activeUnits.Count > 0)
+        {
+            SendUnitToFlag();
+        }
+    }
 
-	private void MakeNewStep()
-	{
-		if (_flag == null && _resourcesCount >= _unitPrice)
-		{
-			BuyNewUnit();
-		}
-		else if (_resourcesCount >= _baseBuildPrice && _activeUnits.Count > 0)
-		{
-			SendUnitToFlag();
-		}
-	}
+    private void TryPlaceFlag()
+    {
+        if (_selectedFlag != null && _flagSelector.FlagSelected)
+        {
+            StopCoroutine(_mouseFollowCoroutine);
 
-	private void TryPlaceFlag()
-	{
-		if (_selectedFlag != null && _flagSelected)
-		{
-			StopCoroutine(_mouseFollowCoroutine);
+            if (_flag != null)
+                Destroy(_flag.gameObject);
 
-			if (_flag != null)
-				Destroy(_flag.gameObject);
+            _flag = Instantiate(_flagPrefab, _ground.HitPosition, Quaternion.identity);
+            Destroy(_selectedFlag.gameObject);
 
-			_flag = Instantiate(_flagPrefab, _ground.HitPosition, Quaternion.identity);
-			Destroy(_selectedFlag.gameObject);
-			_flagSelected = false;
-		}
-	}
+            _flagSelector.SetFlagStatus(false);
+        }
+    }
 
-	private void BuyNewUnit()
-	{
-		CreateUnit();
-		_resourcesCount -= _unitPrice;
-	}
+    private void BuyNewUnit()
+    {
+        CreateUnit();
+        _resourcesCount -= _unitPrice;
+    }
 
-	private IEnumerator SendUnitsForResources()
-	{
-		while (true)
-		{
-			SendUnitToCollectResource();
-			yield return null;
-		}
-	}
+    private IEnumerator SendUnitsForResources()
+    {
+        while (true)
+        {
+            SendUnitToCollectResource();
+            yield return null;
+        }
+    }
 
-	private void SendUnitToCollectResource()
-	{
-		if (_activeUnits.Count > 0 && _resourceScanner.GetResourcesCount() > 0)
-		{
-			_activeUnits.Dequeue().StartDelivery(this, _resourceScanner.GetResource());
-		}
-	}
+    private void SendUnitToCollectResource()
+    {
+        if (_activeUnits.Count > 0 && _resourceScanner.GetResourcesCount() > 0)
+        {
+            _activeUnits.Dequeue().StartDelivery(this, _resourceScanner.GetResource());
+        }
+    }
 
-	private void SendUnitToFlag()
-	{
-		_activeUnits.Dequeue().StartBaseBuilding(this, _flag);
-		_resourcesCount -= _baseBuildPrice;
-		_flag = null;
-	}
+    private void SendUnitToFlag()
+    {
+        _activeUnits.Dequeue().StartBaseBuilding(this, _flag);
+        _resourcesCount -= _baseBuildPrice;
+        _flag = null;
+    }
 
-	private void CreateUnit()
-	{
-		Unit unit = Instantiate(_unitPrefab,
-			_unitSpawnPoints[_unitSpawnPointIndex++ % _unitSpawnPoints.Length].transform.position,
-			Quaternion.identity);
+    private void CreateUnit()
+    {
+        Unit unit = Instantiate(_unitPrefab,
+            _unitSpawnPoints[_unitSpawnPointIndex++ % _unitSpawnPoints.Length].transform.position,
+            Quaternion.identity);
 
-		_activeUnits.Enqueue(unit);
-	}
+        _activeUnits.Enqueue(unit);
+    }
 }
--- Assets/Scripts/FlagSelector.cs
+++ Assets/Scripts/FlagSelector.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 
 public class FlagSelector : MonoBehaviour
 {
-	public bool FlagSelected { get; private set; }
+    public bool FlagSelected { get; private set; } = false;
 
-	private void Awake()
-	{
-		FlagSelected = false;
-	}
-
-
+    public void SetFlagStatus(bool value)
+    {
+        FlagSelected = value;
+    }
 }
